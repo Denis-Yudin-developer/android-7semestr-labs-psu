c793af7194f75627467b15b070585c1788a0cefc
denis.yudin <rng48322@gmail.com>
2023-03-23 20:52:44 +0300

    Добавил sharedPreference, а также сохранение данных при переключении между экранами

--- app/src/main/java/com/example/android_lab_1/TableActivity.java
+++ app/src/main/java/com/example/android_lab_1/TableActivity.java
@@ -1,7 +1,7 @@
 package com.example.android_lab_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,16 +14,21 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class TableActivity extends AppCompatActivity {
-
-    ArrayList<String> entries = new ArrayList<String>();
-    ArrayList<String> selectedEntries = new ArrayList<String>();
+public class TableActivity extends MainActivity {
+    ArrayList<String> entries = new ArrayList<>();
+    ArrayList<String> selectedEntries = new ArrayList<>();
     ArrayAdapter<String> adapter;
     ListView entriesList;
+
+    private SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        restoreLocale();
         setContentView(R.layout.activity_table);
+
+        sharedPref = this.getSharedPreferences("table", Context.MODE_PRIVATE);
 
         Collections.addAll(entries);
 
@@ -51,11 +56,13 @@ public class TableActivity extends AppCompatActivity {
 
     @Override
     protected void onStop(){
+        saveTable();
         super.onStop();
         Log.i("AppLogger", "Переопределение onStop у TableActivity");
     }
     @Override
     protected void onStart(){
+        restoreTable();
         super.onStart();
         Log.i("AppLogger", "Переопределение onStart у TableActivity");
     }
@@ -87,6 +94,7 @@ public class TableActivity extends AppCompatActivity {
         }
     }
     public void removeEntrie(View view){
+
         if(selectedEntries.size() == 0 && entries.size() != 0) {
             int numOfLastEntrie = entries.size() - 1;
             adapter.remove(entries.get(numOfLastEntrie));
@@ -100,4 +108,29 @@ public class TableActivity extends AppCompatActivity {
 
         adapter.notifyDataSetChanged();
     }
+
+    private void saveTable(){
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putInt("table_size", entries.size());
+        for(int i=0;i < entries.size(); i++)
+        {
+            editor.remove("table_ent_" + i);
+            editor.putString("table_ent_" + i, entries.get(i));
+        }
+
+        editor.apply();
+    }
+
+    private void restoreTable()
+    {
+        entries.clear();
+        int size = sharedPref.getInt("table_size", 0);
+        for(int i=0;i < size;i++)
+        {
+            entries.add(sharedPref.getString("table_ent_" + i, null));
+        }
+
+    }
+
 }
